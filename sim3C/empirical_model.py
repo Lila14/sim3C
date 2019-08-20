e9187665da62b0ebfbf7c26486bf7e572e45b126
Lila <lur159@psu.edu>
2019-08-20 15:29:34 -0400

    testing

--- sim3C/empirical_model.py
+++ sim3C/empirical_model.py
@@ -230,10 +230,6 @@ def _random_nested_intervals(random_state, result, inv, min_len, max_len, min_nu
 
 def nonrandom_nested_intervals(subseqs, depths):
     result = []
-    logger.info("subseqs")
-    logger.info(subseqs)
-    logger.info("depths")
-    logger.info(depths)
     for subseq, depth in zip(subseqs, depths):
         subseq = np.array(subseq)
         # adjacent elements become the next level of intervals
@@ -270,6 +266,8 @@ def generate_nested_cids(random_state, chr_length, chr_prob, chr_bins, chr_shape
     if depths is None or subseqs is None:
         _random_nested_intervals(random_state, cid_list, top_inv, min_len, max_len, min_num, max_num, recur_depth)
     else:
+        logger.info("subseqs: {}".format(subseqs))
+        logger.info("depths: {}".format(depths))
         cid_list = nonrandom_nested_intervals(np.loadtxt(subseqs), np.loadtxt(depths))
 
     # flatten returned list of intervals
